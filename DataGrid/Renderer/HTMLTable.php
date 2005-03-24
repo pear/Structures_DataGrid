3eff54fc9df965a97e05572e053330f30b50af0a
Andrew Nagy <asnagy@php.net>
2005-03-24 16:46:55 +0000

    getTable now returns an error if fetchData returns an error
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@182793 c90b9560-bf6c-de11-be94-00142212c4b1

--- DataGrid/Renderer/HTMLTable.php
+++ DataGrid/Renderer/HTMLTable.php
@@ -258,7 +258,9 @@ class Structures_DataGrid_Renderer_HTMLTable
 
         if (!$this->_rendered) {
             // Get the data to be rendered
-            $dg->fetchDataSource();
+            if (PEAR::isError($result = $dg->fetchDataSource())) {
+                return $result;
+            }
 
             // Check to see if column headers exist, if not create them
             // This must follow after any fetch method call
