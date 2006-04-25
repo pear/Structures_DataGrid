d38bd58dd98f4ff39a3a281bac2cba82b44d9b81
Mark Wiesemann <wiesemann@php.net>
2006-04-25 08:58:30 +0000

    - batch files as helpers for the new releases
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@211965 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package-validate.bat
@@ -0,0 +1,20 @@
+@echo off
+
+call pear package-validate package2.xml
+call pear package-validate package2-ds-array.xml
+call pear package-validate package2-ds-csv.xml
+call pear package-validate package2-ds-db.xml
+call pear package-validate package2-ds-dbdataobject.xml
+call pear package-validate package2-ds-dbquery.xml
+call pear package-validate package2-ds-dbtable.xml
+call pear package-validate package2-ds-mdb2.xml
+call pear package-validate package2-ds-rss.xml
+call pear package-validate package2-ds-xml.xml
+call pear package-validate package2-r-console.xml
+call pear package-validate package2-r-csv.xml
+call pear package-validate package2-r-htmltable.xml
+call pear package-validate package2-r-pager.xml
+call pear package-validate package2-r-smarty.xml
+call pear package-validate package2-r-xls.xml
+call pear package-validate package2-r-xml.xml
+call pear package-validate package2-r-xul.xml
