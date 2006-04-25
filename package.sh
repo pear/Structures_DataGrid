9dd1e2f684ce0d829dab0d3f10e9ea17764d9285
Olivier Guilyardi <olivierg@php.net>
2006-04-25 18:37:14 +0000

    Added shell equivalents of packaging batch scripts
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@211979 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pear package package2.xml
+pear package package2-ds-array.xml
+pear package package2-ds-csv.xml
+pear package package2-ds-db.xml
+pear package package2-ds-dbdataobject.xml
+pear package package2-ds-dbquery.xml
+pear package package2-ds-dbtable.xml
+pear package package2-ds-mdb2.xml
+pear package package2-ds-rss.xml
+pear package package2-ds-xml.xml
+pear package package2-r-console.xml
+pear package package2-r-csv.xml
+pear package package2-r-htmltable.xml
+pear package package2-r-pager.xml
+pear package package2-r-smarty.xml
+pear package package2-r-xls.xml
+pear package package2-r-xml.xml
+pear package package2-r-xul.xml
+
