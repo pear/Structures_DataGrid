6ba935c046df9dcda36cee039900bfe0dbd6c83e
Olivier Guilyardi <olivierg@php.net>
2006-06-15 16:10:05 +0000

    now display readable output
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@214846 c90b9560-bf6c-de11-be94-00142212c4b1

--- package-validate.sh
+++ package-validate.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-pear package-validate package2.xml
-pear package-validate package2-ds-array.xml
-pear package-validate package2-ds-csv.xml
-pear package-validate package2-ds-db.xml
-pear package-validate package2-ds-dbdataobject.xml
-pear package-validate package2-ds-dbquery.xml
-pear package-validate package2-ds-dbtable.xml
-pear package-validate package2-ds-mdb2.xml
-pear package-validate package2-ds-rss.xml
-pear package-validate package2-ds-xml.xml
-pear package-validate package2-r-console.xml
-pear package-validate package2-r-csv.xml
-pear package-validate package2-r-htmltable.xml
-pear package-validate package2-r-pager.xml
-pear package-validate package2-r-smarty.xml
-pear package-validate package2-r-xls.xml
-pear package-validate package2-r-xml.xml
-pear package-validate package2-r-xul.xml
-pear package-validate package2-r-htmlsortform.xml
-
+for f in package2*.xml; do 
+    printf "Checking $f... "
+    if pear package-validate $f > /dev/null; then
+        echo OK
+    else
+        echo FAILED
+        echo ---------------------------
+        pear package-validate $f 
+        echo ---------------------------
+    fi
+done    
