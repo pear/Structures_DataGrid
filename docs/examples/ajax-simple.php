0ca9a5af94281f01a030058e40c2ff95f538c05b
Olivier Guilyardi <olivierg@php.net>
2007-06-19 13:54:24 +0000

    add simple ajax example
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@238062 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/examples/ajax-simple.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'PEAR.php';
+require_once 'Structures/DataGrid.php';    
+
+$datagrid =& new Structures_DataGrid(10);
+
+$options['dsn'] = 'mysql://username@localhost/mydatabase';
+$datagrid->bind("SELECT * FROM mytable", $options);
+
+// Set the javascript handler function for onclick events
+$datagrid->setRendererOption('jsHandler', 'updateGrid', true);
+
+// Handle table AJAX requests 
+if (@$_GET['ajax'] == 'table') {
+    $datagrid->render();
+    exit();
+}
+
+// Handle pager AJAX requests 
+if (@$_GET['ajax'] == 'pager') {
+    // Warning: require Pager CVS or above 2.4.3
+    $datagrid->render('Pager');
+    exit();
+}
+
+// No AJAX request, render the initial content..
+?>
+<html>
+
+<head>
+<!-- Require the Prototype JS framework from http://www.prototypejs.org -->
+<script type="text/javascript" src="prototype.js"></script>
+<script type="text/javascript">
+function updateGrid(info) 
+{
+    var url = '<?=$_SERVER['PHP_SELF']?>';
+    var pars = 'page=' + info.page;
+    if (info.sort.length > 0) {
+        pars += '&orderBy=' + info.sort[0].field + '&direction=' + info.sort[0].direction;
+    }
+        
+    new Ajax.Updater( 'grid', url, { method: 'get', parameters: pars + '&ajax=table' });
+    new Ajax.Updater( 'pager', url, { method: 'get', parameters: pars + '&ajax=pager' });
+
+    // Important: return false to void href links
+    return false;
+}
+</script>
+</head>
+
+<body>
+Pages: <span id="pager"><?php $datagrid->render('Pager'); ?></span>
+<div id="grid"><? $datagrid->render(); ?></div>
+</body>
+
+</html>
