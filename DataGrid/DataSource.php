1b06ce09afa2cc6b00b51cf09ecba5080a80a0e4
Mark Wiesemann <wiesemann@php.net>
2008-03-26 20:27:24 +0000

    - Bug #13339: Automatic ORDER BY sort string addition can fail if subqueries   are used in the the DBQuery, MDB2, and PDO DataSource drivers - update changelog
    git-svn-id: http://svn.php.net/repository/pear/packages/Structures_DataGrid/trunk@256139 c90b9560-bf6c-de11-be94-00142212c4b1

--- DataGrid/DataSource.php
+++ DataGrid/DataSource.php
@@ -606,13 +606,20 @@ class Structures_DataGrid_DataSource_SQLQuery
 
         // if we have a sort string, we need to add it to the query string
         if ($sortString != '') {
-            // if there is an existing ORDER BY statement, we can just add the
-            // sort string
-            $result = preg_match('#ORDER\s+BY#is', $query);
-            if ($result === 1) {
-                $query .= ', ' . $sortString;
-            } else {  // otherwise we need to specify 'ORDER BY'
+            $appendOrderBy = false;
+            // search for the last ORDER BY statement
+            $orderByPos = strripos($query, 'ORDER BY');
+            // does another (sub)query follow after this ORDER BY statement?
+            if (   $orderByPos !== false
+                && strpos($query, 'SELECT', $orderByPos) !== false
+            ) {
+                // yes => new ORDER BY statement needs to be appended
+                $appendOrderBy = true;
+            }
+            if ($appendOrderBy === true) {
                 $query .= ' ORDER BY ' . $sortString;
+            } else {
+                $query .= ', ' . $sortString;
             }
         }
 
@@ -714,6 +721,70 @@ class Structures_DataGrid_DataSource_SQLQuery
         }
     }
 
+}
+
+/**
+ * Replace strripos()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @link        http://php.net/function.strripos
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     $Revision$
+ * @since       PHP 5
+ * @require     PHP 4.0.0 (user_error)
+ */
+function php_compat_strripos($haystack, $needle, $offset = null)
+{
+    // Sanity check
+    if (!is_scalar($haystack)) {
+        user_error('strripos() expects parameter 1 to be scalar, ' .
+            gettype($haystack) . ' given', E_USER_WARNING);
+        return false;
+    }
+
+    if (!is_scalar($needle)) {
+        user_error('strripos() expects parameter 2 to be scalar, ' .
+            gettype($needle) . ' given', E_USER_WARNING);
+        return false;
+    }
+
+    if (!is_int($offset) && !is_bool($offset) && !is_null($offset)) {
+        user_error('strripos() expects parameter 3 to be long, ' .
+            gettype($offset) . ' given', E_USER_WARNING);
+        return false;
+    }
+
+    // Initialise variables
+    $needle         = strtolower($needle);
+    $haystack       = strtolower($haystack);
+    $needle_fc      = $needle{0};
+    $needle_len     = strlen($needle);
+    $haystack_len   = strlen($haystack);
+    $offset         = (int) $offset;
+    $leftlimit      = ($offset >= 0) ? $offset : 0;
+    $p              = ($offset >= 0) ?
+                            $haystack_len :
+                            $haystack_len + $offset + 1;
+
+    // Reverse iterate haystack
+    while (--$p >= $leftlimit) {
+        if ($needle_fc === $haystack{$p} &&
+            substr($haystack, $p, $needle_len) === $needle) {
+            return $p;
+        }
+    }
+
+    return false;
+}
+
+if (!function_exists('strripos')) {
+    function strripos($haystack, $needle, $offset = null)
+    {
+        return php_compat_strripos($haystack, $needle, $offset);
+    }
 }
 
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
